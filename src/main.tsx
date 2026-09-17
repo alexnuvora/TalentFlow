97b2ad7ffdaa2ee249d78e70698599217c723823
alexnuvora <alex.nuvora@gmail.com>
2026-09-17 05:14:33 +0100

    Add search and answer-engine optimization foundation

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import React from 'react';import {createRoot} from 'react-dom/client';import App
 import './careers.css';
 import './v9.css';
 import './marketing.css';
+import './customer.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
