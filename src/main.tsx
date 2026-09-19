7e1e8897c0ab7ebd0f42a986e4bdda3a5d424938
alexnuvora <alex.nuvora@gmail.com>
2026-09-19 05:09:18 +0100

    fix: restore targeted dashboard styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,6 @@ import './marketing.css';
 import './careers.css';
 import './customer.css';
 import './v9.css';
+import './dashboard-fixes.css';
 
 createRoot(document.getElementById('root')!).render(<React.StrictMode><ErrorBoundary><ToastProvider><App/></ToastProvider></ErrorBoundary></React.StrictMode>);
