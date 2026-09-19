0801dc882fcccbc8570e002a00ba36be9578a0e8
alexnuvora <alex.nuvora@gmail.com>
2026-09-19 04:45:16 +0100

    feat: enable global toast notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App';
+import App from './App';import {ToastProvider} from './components/Ui';
 import {ErrorBoundary} from './components/ErrorBoundary';
 import './styles.css';
 import './marketing.css';
@@ -8,4 +8,4 @@ import './careers.css';
 import './customer.css';
 import './v9.css';
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><ErrorBoundary><App/></ErrorBoundary></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><ErrorBoundary><ToastProvider><App/></ToastProvider></ErrorBoundary></React.StrictMode>);
