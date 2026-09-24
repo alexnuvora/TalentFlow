484aea499fde6cd64faa5050bd09ee5460889ac0
alexnuvora <alex.nuvora@gmail.com>
2026-09-24 22:02:35 +0100

    Keep partner attribution entity integrity on client delete

---
+++ supabase/migrations/20260924222000_cascade_client_partner_attribution_on_client_delete.sql
@@ -0,0 +1,8 @@
+alter table public.partner_attributions
+  drop constraint if exists partner_attributions_client_id_fkey;
+
+alter table public.partner_attributions
+  add constraint partner_attributions_client_id_fkey
+  foreign key (client_id)
+  references public.clients(id)
+  on delete cascade;
